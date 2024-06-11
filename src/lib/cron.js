1cabf1a8f99347e18d3d356276aff2771381e47f
AntoineGregoire <84733479+AntoineGregoire@users.noreply.github.com>
2024-06-11 10:48:41 -0400

    First commit of the day

--- src/lib/cron.js
+++ src/lib/cron.js
@@ -1,30 +1,30 @@
 // // lib/cron.js
-// import cron from 'node-cron';
-// import prisma from "$lib/prisma";
+import cron from 'node-cron';
+import prisma from "$lib/prisma";
 
 
-// // Schedule the cron job to run at midnight every day
-// cron.schedule('55 15 * * *', async () => {
-//   console.log('Running a task every day');
-//   await logOutUsers();
-// });
+// Schedule the cron job to run at midnight every day
+cron.schedule('55 10 * * *', async () => {
+  console.log('Running a task every day');
+  await logOutUsers();
+});
 
-// async function logOutUsers() {
-//   try {
-//     // Update all users to set loggedIn to false
-//     await prisma.user.updateMany({
-//       data: {
-//         loggedIn: false
-//       }
-//     });
-//     console.log('All users logged out');
-//   } catch (error) {
-//     console.error('Failed to log out users:', error);
+async function logOutUsers() {
+  try {
+    // Update all users to set loggedIn to false
+    await prisma.user.updateMany({
+      data: {
+        loggedIn: true
+      }
+    });
+    console.log('All users logged out');
+  } catch (error) {
+    console.error('Failed to log out users:', error);
+  }
+//   } finally {
+//     await prisma.$disconnect();
 //   }
-// //   } finally {
-// //     await prisma.$disconnect();
-// //   }
-// }
+}
 
-// // Initialize the cron job
-// logOutUsers();
+// Initialize the cron job
+logOutUsers();
