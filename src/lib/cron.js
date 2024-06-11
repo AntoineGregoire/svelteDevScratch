ff720e357dd3aac128ff24ec2638fb6eb78f3fc6
AntoineGregoire <84733479+AntoineGregoire@users.noreply.github.com>
2024-06-11 12:03:41 -0400

    Summary

--- src/lib/cron.js
+++ src/lib/cron.js
@@ -4,7 +4,7 @@ import prisma from "$lib/prisma";
 
 
 // Schedule the cron job to run at midnight every day
-cron.schedule('59 11 * * *', async () => {
+cron.schedule('07 12 * * *', async () => {
   console.log('Running a task every day');
   await logOutUsers();
 });
@@ -12,11 +12,11 @@ cron.schedule('59 11 * * *', async () => {
 async function logOutUsers() {
   try {
     // Update all users to set loggedIn to false
-    // await prisma.user.updateMany({
-    //   data: {
-    //     loggedIn: false
-    //   }
-    // });
+    await prisma.user.updateMany({
+      data: {
+        loggedIn: false
+      }
+    });
     console.log('All users logged out');
   } catch (error) {
     console.error('Failed to log out users:', error);
